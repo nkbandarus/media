3f0c4c9d662ee68d6214edacbe1a2e4efbe1322c
Naresh Bandaru (contractor) <naresh.bandaru@macys.com>
2019-12-08 13:54:52 -0800

    Adding lombok annotationProcessor plugin to fix getter/setter annotations

--- src/main/java/com/binge/data/User.java
+++ src/main/java/com/binge/data/User.java
@@ -1,5 +1,6 @@
 package com.binge.data;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -7,33 +8,13 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Document
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable {
     @Id
     private String userId;
     private String name;
     private Date creationDate = new Date();
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
 }
